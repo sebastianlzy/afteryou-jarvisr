f394f7f22d0ecd69c2935e42fd0a75ca97e4c714
sebastian <syang@rocketfuel.com>
2014-11-28 14:46:02 +0800

    added room with areas

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 (function () {
   'use strict';
   var Hapi, Good, gulp, _request, _, prettyjson, server, service_reports, service_report_134,
-    _listings, _rooms;
+    _listings, _rooms, _roomsWithAreas;
   Hapi = require('hapi');
   Good = require('good');
   gulp = require('gulp');
@@ -14,6 +14,7 @@
 
   _listings = require('./listings.js');
   _rooms = require('./rooms.js');
+  _roomsWithAreas = require('./roomWithAreas');
 
   server = Hapi.createServer('0.0.0.0', 3000, {json: { space: 2 }, cors: true, router: { stripTrailingSlash: true }});
 
@@ -396,277 +397,19 @@
 //JARVIS
   var rooms = function (id) {
     var rooms = _rooms.getAll();
-
-
     if (isNaN(id)) {
 
       return rooms
     }
-
     return _.find(rooms, { id: parseInt(id) });
-
-
   }
 
   var roomWithAreas = function (id) {
-    var roomsWithAreas = [
-      {
-        "template_areas": [
-          {
-            "id": 2,
-            "name": "Sofa Set"
-          },
-          {
-            "id": 3,
-            "name": "Coffee Table"
-          },
-          {
-            "id": 4,
-            "name": "Sideboard"
-          },
-          {
-            "id": 5,
-            "name": "Bar Counter"
-          },
-          {
-            "id": 6,
-            "name": "Display Cabinet"
-          },
-          {
-            "id": 7,
-            "name": "Television"
-          },
-          {
-            "id": 8,
-            "name": "Telephone"
-          },
-          {
-            "id": 9,
-            "name": "Chandelier"
-          },
-          {
-            "id": 10,
-            "name": "Ceiling Light"
-          },
-          {
-            "id": 11,
-            "name": "Wall Light"
-          },
-          {
-            "id": 12,
-            "name": "Air Conditioner"
-          },
-          {
-            "id": 13,
-            "name": "Fan"
-          },
-          {
-            "id": 14,
-            "name": "Shoe Rack"
-          },
-          {
-            "id": 15,
-            "name": "Window Curtains/Blinds"
-          }
-        ],
-        "id": 5,
-        "type": "Living Room",
-        "default_number": 1
-      },
-      {
-        "template_areas": [
-          {
-            "id": 10,
-            "name": "Ceiling Light"
-          },
-          {
-            "id": 11,
-            "name": "Wall Light"
-          },
-          {
-            "id": 12,
-            "name": "Air Conditioner"
-          },
-          {
-            "id": 13,
-            "name": "Fan"
-          },
-          {
-            "id": 15,
-            "name": "Window Curtains/Blinds"
-          },
-          {
-            "id": 16,
-            "name": "Bed & Mattress"
-          },
-          {
-            "id": 17,
-            "name": "Wardrobe"
-          },
-          {
-            "id": 18,
-            "name": "Dressing Table"
-          },
-          {
-            "id": 19,
-            "name": "Chair/Stool"
-          },
-          {
-            "id": 20,
-            "name": "Side Table"
-          }
-        ],
-        "id": 6,
-        "type": "Bedroom",
-        "default_number": 2
-      },
-      {
-        "template_areas": [
-          {
-            "id": 10,
-            "name": "Ceiling Light"
-          },
-          {
-            "id": 11,
-            "name": "Wall Light"
-          },
-          {
-            "id": 13,
-            "name": "Fan"
-          },
-          {
-            "id": 21,
-            "name": "Dining Table"
-          },
-          {
-            "id": 22,
-            "name": "Dining Chairs"
-          },
-          {
-            "id": 23,
-            "name": "Kitchen Cabinet"
-          },
-          {
-            "id": 24,
-            "name": "Cooker Hood"
-          },
-          {
-            "id": 25,
-            "name": "Cooker Hob"
-          },
-          {
-            "id": 26,
-            "name": "Refrigerator"
-          },
-          {
-            "id": 27,
-            "name": "Microwave Oven"
-          },
-          {
-            "id": 28,
-            "name": "Oven"
-          },
-          {
-            "id": 29,
-            "name": "Washing Machine"
-          }
-        ],
-        "id": 7,
-        "type": "Kitchen",
-        "default_number": 1
-      },
-      {
-        "template_areas": [
-          {
-            "id": 10,
-            "name": "Ceiling Light"
-          },
-          {
-            "id": 11,
-            "name": "Wall Light"
-          },
-          {
-            "id": 30,
-            "name": "Water Heater"
-          },
-          {
-            "id": 31,
-            "name": "Shower Curtain"
-          },
-          {
-            "id": 32,
-            "name": "Cabinet"
-          },
-          {
-            "id": 33,
-            "name": "Toilet Bowl"
-          },
-          {
-            "id": 34,
-            "name": "Sink & Tap"
-          },
-          {
-            "id": 35,
-            "name": "Bathtub"
-          }
-        ],
-        "id": 8,
-        "type": "Bathroom",
-        "default_number": 2
-      },
-      {
-        "template_areas": [
-          {
-            "id": 10,
-            "name": "Ceiling Light"
-          },
-          {
-            "id": 11,
-            "name": "Wall Light"
-          },
-          {
-            "id": 15,
-            "name": "Window Curtains/Blinds"
-          },
-          {
-            "id": 36,
-            "name": "Patio Table"
-          },
-          {
-            "id": 37,
-            "name": "Patio Chairs"
-          }
-        ],
-        "id": 9,
-        "type": "Balcony/Patio",
-        "default_number": 0
-      },
-      {
-        "template_areas": [
-          {
-            "id": 10,
-            "name": "Ceiling Light"
-          },
-          {
-            "id": 11,
-            "name": "Wall Light"
-          },
-          {
-            "id": 38,
-            "name": "Cabinet/Shelf"
-          }
-        ],
-        "id": 10,
-        "type": "Store Room",
-        "default_number": 1
-      }
-    ];
-
+    var roomsWithAreas = _roomsWithAreas.getAll();
     if (isNaN(id)) {
       return roomsWithAreas;
     }
     return _.find(roomsWithAreas, { id: parseInt(id) });
-
   }
 
   var areas = function (id) {
---
+++ roomWIthAreas.js
@@ -0,0 +1,276 @@
+
+
+/**
+ * Created by sebastianlee on 11/28/14.
+ *
+ */
+
+/*global exports */
+exports.getAll = function () {
+  'use strict';
+  return [
+    {
+      "template_areas": [
+        {
+          "id": 10,
+          "name": "Ceiling Light"
+        },
+        {
+          "id": 11,
+          "name": "Wall Light"
+        },
+        {
+          "id": 12,
+          "name": "Air Conditioner"
+        },
+        {
+          "id": 13,
+          "name": "Fan"
+        },
+        {
+          "id": 15,
+          "name": "Window Curtains/Blinds"
+        },
+        {
+          "id": 16,
+          "name": "Bed & Mattress"
+        },
+        {
+          "id": 17,
+          "name": "Wardrobe"
+        },
+        {
+          "id": 18,
+          "name": "Dressing Table"
+        },
+        {
+          "id": 19,
+          "name": "Chair/Stool"
+        },
+        {
+          "id": 20,
+          "name": "Side Table"
+        }
+      ],
+      "id": 6,
+      "type": "Bedroom",
+      "default_number": 2,
+      "position": 1
+    },
+    {
+      "template_areas": [
+        {
+          "id": 10,
+          "name": "Ceiling Light"
+        },
+        {
+          "id": 11,
+          "name": "Wall Light"
+        },
+        {
+          "id": 30,
+          "name": "Water Heater"
+        },
+        {
+          "id": 31,
+          "name": "Shower Curtain"
+        },
+        {
+          "id": 32,
+          "name": "Cabinet"
+        },
+        {
+          "id": 33,
+          "name": "Toilet Bowl"
+        },
+        {
+          "id": 35,
+          "name": "Bathtub"
+        },
+        {
+          "id": 34,
+          "name": "Sink & Tap"
+        }
+      ],
+      "id": 8,
+      "type": "Bathroom",
+      "default_number": 2,
+      "position": 2
+    },
+    {
+      "template_areas": [
+        {
+          "id": 2,
+          "name": "Sofa Set"
+        },
+        {
+          "id": 3,
+          "name": "Coffee Table"
+        },
+        {
+          "id": 4,
+          "name": "Sideboard"
+        },
+        {
+          "id": 5,
+          "name": "Bar Counter"
+        },
+        {
+          "id": 6,
+          "name": "Display Cabinet"
+        },
+        {
+          "id": 7,
+          "name": "Television"
+        },
+        {
+          "id": 8,
+          "name": "Telephone"
+        },
+        {
+          "id": 9,
+          "name": "Chandelier"
+        },
+        {
+          "id": 10,
+          "name": "Ceiling Light"
+        },
+        {
+          "id": 11,
+          "name": "Wall Light"
+        },
+        {
+          "id": 12,
+          "name": "Air Conditioner"
+        },
+        {
+          "id": 13,
+          "name": "Fan"
+        },
+        {
+          "id": 14,
+          "name": "Shoe Rack"
+        },
+        {
+          "id": 15,
+          "name": "Window Curtains/Blinds"
+        }
+      ],
+      "id": 5,
+      "type": "Living Room",
+      "default_number": 1,
+      "position": 3
+    },
+    {
+      "template_areas": [
+        {
+          "id": 10,
+          "name": "Ceiling Light"
+        },
+        {
+          "id": 11,
+          "name": "Wall Light"
+        },
+        {
+          "id": 13,
+          "name": "Fan"
+        },
+        {
+          "id": 21,
+          "name": "Dining Table"
+        },
+        {
+          "id": 22,
+          "name": "Dining Chairs"
+        },
+        {
+          "id": 23,
+          "name": "Kitchen Cabinet"
+        },
+        {
+          "id": 24,
+          "name": "Cooker Hood"
+        },
+        {
+          "id": 25,
+          "name": "Cooker Hob"
+        },
+        {
+          "id": 26,
+          "name": "Refrigerator"
+        },
+        {
+          "id": 27,
+          "name": "Microwave Oven"
+        },
+        {
+          "id": 28,
+          "name": "Oven"
+        },
+        {
+          "id": 29,
+          "name": "Washing Machine"
+        }
+      ],
+      "id": 7,
+      "type": "Kitchen",
+      "default_number": 1,
+      "position": 4
+    },
+    {
+      "template_areas": [
+        {
+          "id": 10,
+          "name": "Ceiling Light"
+        },
+        {
+          "id": 11,
+          "name": "Wall Light"
+        },
+        {
+          "id": 38,
+          "name": "Cabinet/Shelf"
+        }
+      ],
+      "id": 10,
+      "type": "Store Room",
+      "default_number": 1,
+      "position": 5
+    },
+    {
+      "template_areas": [
+        {
+          "id": 10,
+          "name": "Ceiling Light"
+        },
+        {
+          "id": 11,
+          "name": "Wall Light"
+        },
+        {
+          "id": 15,
+          "name": "Window Curtains/Blinds"
+        },
+        {
+          "id": 36,
+          "name": "Patio Table"
+        },
+        {
+          "id": 37,
+          "name": "Patio Chairs"
+        }
+      ],
+      "id": 9,
+      "type": "Balcony/Patio",
+      "default_number": 0,
+      "position": 6
+    },
+    {
+      "template_areas": [ ],
+      "id": 11,
+      "type": "Others",
+      "default_number": 0,
+      "position": 7
+    }
+  ];
+};
