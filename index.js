b9306402ba188083b6ff53d5eb4159e5c31dcdab
sebastian <syang@rocketfuel.com>
2014-12-05 14:08:21 +0800

    added capability to save image

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-
 /*global require, console*/
 
 (function () {
   'use strict';
   var Hapi, Good, _request, _, prettyjson, server, fs,
     getPath, postPath, putPath,
-    rooms, roomWithAreas, areas, areaWithIssues, listing, serviceReports, messages,
+    rooms, roomWithAreas, areas, areaWithIssues, listing, serviceReports,
     _listings, _rooms, _roomsWithAreas, _areas, _areasWithIssues, _serviceReports, _messages,
     postLogin, postListing, postRequest, putServiceReport, postNotificationToken, postFeedback, postChangePassword, postSRmail, postMessageServiceReport,
-    getFromOneMap, getStreetName, token, renewToken = false ;
+    getFromOneMap, getStreetName, token, renewToken = false;
 
   //NODE METHOD
   Hapi = require('hapi');
@@ -17,7 +16,7 @@
   fs = require('fs');
   prettyjson = require('prettyjson');
   _request = require('request');
-  server = Hapi.createServer('0.0.0.0', 3000, {json: { space: 2 }, cors: true, router: { stripTrailingSlash: true }});
+  server = Hapi.createServer('0.0.0.0', 3000, { json: { space: 2 }, cors: true, router: { stripTrailingSlash: true } });
 
   //JARVIS OBJECTS
   _listings = require('./listings.js');
@@ -27,7 +26,7 @@
   _areasWithIssues = require('./areasWithIssues');
   _serviceReports = require('./serviceReports');
   _messages = [
-      {
+    {
       "user": {
         "id": 12,
         "username": "deepan@afteryou.co"
@@ -35,8 +34,8 @@
       "id": 50,
       "content": "Good bye",
       "creation_date": "01/12/2014 17:01"
-      },
-      {
+    },
+    {
       "user": {
         "id": 11,
         "username": "sebastian@afteryou.co"
@@ -44,19 +43,15 @@
       "id": 49,
       "content": "Hello",
       "creation_date": "01/12/2014 17:01"
-      }
+    }
   ];
-
   //HANDLER METHODS
   getPath = function (path, handler) {
-
     server.route({
       method: 'GET',
       path: path,
       handler: handler
-
     });
-
   };
   postPath = function (path, handler) {
     server.route({
@@ -72,8 +67,6 @@
       handler: handler
     });
   };
-
-
   //ONE MAP
   getFromOneMap = function (searchValue, reply, token) {
     var searchResult, coordinateX, coordinateY;
@@ -118,7 +111,6 @@
     function getOneMapToken(accessKey) {
       return 'http://www.onemap.sg/API/services.svc/getToken?accessKEY=' + accessKey;
     }
-
     var accessKey, searchValue;
     searchValue = request.params.searchValue;
     accessKey = 'J4sIOv0L4uclNQIYVlzmRJJUi1hajGYFF/fpL5quwMjgXz3zo8UpjFAz/zRsZkVLrco3KmgW7mTpaHRVSZDYVN57oQzzD/Of|mv73ZvjFcSo=';
@@ -129,17 +121,14 @@
           token = JSON.parse(body).GetToken[0].NewToken;
           renewToken = false;
           getFromOneMap(searchValue, reply, token);
-
         } else {
           console.log(error);
         }
       });
     } else {
       getFromOneMap(searchValue, reply, token);
-
     }
   };
-
   //GET METHODS
   rooms = function (id) {
     if (isNaN(id)) {
@@ -155,7 +144,6 @@
   };
   areas = function (id) {
     if (isNaN(id)) {
-
       return _areas.getAll();
     }
     return _.find(_areas.getAll(), { id: +id });
@@ -175,28 +163,41 @@
     }
     return _serviceReports.getServiceReport(+id);
   };
-
-
   //GET PATHS
-  getPath("/agent_app/template/areas/{id?}", function (request, reply) { reply(areas(encodeURIComponent(request.params.id))); });
-  getPath("/agent_app/template/areas/issues", function (request, reply) { reply(areaWithIssues()); });
-  getPath("/agent_app/template/areas/{id}/issues", function (request, reply) { reply(areaWithIssues(encodeURIComponent(request.params.id))); });
-  getPath("/agent_app/template/rooms/{id?}", function (request, reply) { reply(rooms(encodeURIComponent(request.params.id))); });
-  getPath("/agent_app/template/rooms/{id}/areas",  function (request, reply) { reply(roomWithAreas(encodeURIComponent(request.params.id))); });
-  getPath("/agent_app/template/rooms/areas", function (request, reply) { reply(roomWithAreas()); });
-  getPath("/agent_app/listing", function (request, reply) { reply(listing()); });
-  getPath("/agent_app/service_report/{id?}", function (request, reply) { reply(serviceReports(encodeURIComponent(request.params.id))); });
+  getPath("/agent_app/template/areas/{id?}", function (request, reply) {
+    reply(areas(encodeURIComponent(request.params.id)));
+  });
+  getPath("/agent_app/template/areas/issues", function (request, reply) {
+    reply(areaWithIssues());
+  });
+  getPath("/agent_app/template/areas/{id}/issues", function (request, reply) {
+    reply(areaWithIssues(encodeURIComponent(request.params.id)));
+  });
+  getPath("/agent_app/template/rooms/{id?}", function (request, reply) {
+    reply(rooms(encodeURIComponent(request.params.id)));
+  });
+  getPath("/agent_app/template/rooms/{id}/areas", function (request, reply) {
+    reply(roomWithAreas(encodeURIComponent(request.params.id)));
+  });
+  getPath("/agent_app/template/rooms/areas", function (request, reply) {
+    reply(roomWithAreas());
+  });
+  getPath("/agent_app/listing", function (request, reply) {
+    reply(listing());
+  });
+  getPath("/agent_app/service_report/{id?}", function (request, reply) {
+    reply(serviceReports(encodeURIComponent(request.params.id)));
+  });
   getPath("/agent_app/streetName/{searchValue}", getStreetName);
-  getPath("/agent_app/messages/ServiceReport/{id}", function (request,reply){ reply(_messages) });
-
+  getPath("/agent_app/messages/ServiceReport/{id}", function (request, reply) {
+    reply(_messages);
+  });
   //POST METHODS
   function logRequest(request, request_name) {
-
     request_name = request_name || "DEBUG";
     var payload = request.payload;
     console.log('============================ ' + request_name + ' ==========================');
     console.log(prettyjson.render(payload));
-
   }
   postLogin = function (request, reply) {
     var payload = request.payload;
@@ -206,14 +207,13 @@
     } else {
       reply(Hapi.error.badRequest('Invalid Credential'));
     }
-
   };
   postListing = function (request, reply) {
     logRequest(request, 'Listing');
     reply({ id: 4 });
   };
   postRequest = function (request, reply) {
-    logRequest(request, 'Request');
+    //logRequest(request, 'Request');
     reply({ id: 1 });
   };
   putServiceReport = function (request, reply) {
@@ -223,7 +223,6 @@
   postNotificationToken = function (request, reply) {
     reply(request.payload);
     logRequest(request, 'Token');
-
   };
   postFeedback = function (request, reply) {
     logRequest(request, "Feedback");
@@ -236,19 +235,18 @@
   };
   postMessageServiceReport = function (request, reply) {
     var newMessage = {
-    "user": {
-      "id": 11,
-      "username": "sebastian@afteryou.co"
-    },
-    "id": 51,
-    "content": request.payload.content || "HELLO",
-    "creation_date": "01/12/2014 17:01"
+      "user": {
+        "id": 11,
+        "username": "sebastian@afteryou.co"
+      },
+      "id": 51,
+      "content": request.payload.content || "HELLO",
+      "creation_date": "01/12/2014 17:01"
     };
-   _messages.unshift(newMessage);
+    _messages.unshift(newMessage);
     logRequest(request, "Messages Service Report");
     reply(newMessage);
   };
-
   //IMAGE UPLOAD
   server.route({
     method: 'POST',
@@ -258,13 +256,13 @@
         maxBytes: 209715200,
         output: 'stream',
         parse: true
-      },
-      handler: function (request, reply) {
-        request.payload.image.pipe(fs.createWriteStream("image_1"));
       }
+    },
+    handler: function (request, reply) {
+      request.payload.image.pipe(fs.createWriteStream(request.payload.client_image_id + ".jpg"));
+      reply("Received");
     }
   });
-
   //POST PATHS
   postPath("/login", postLogin);
   postPath("/agent_app/listing", postListing);
@@ -274,21 +272,17 @@
   postPath("/change_password", postChangePassword);
   postPath("/agent_app/service_report/send_mail", postSRmail);
   postPath("/agent_app/messages/ServiceReport/{id}", postMessageServiceReport);
-  postPath("/agent_app/debug", function (request) {logRequest(request, "DEBUG"); });
-
-
+  postPath("/agent_app/debug", function (request, reply) {
+    logRequest(request, "DEBUG");
+    reply("good");
+  });
   putPath("/agent_app/service_report/{id}", putServiceReport);
-
-
   server.pack.register(Good, function (err) {
     if (err) {
       throw err; // something bad happened loading the plugin
     }
-
     server.start(function () {
       server.log('info', 'Server running at: ' + server.info.uri);
     });
   });
-
-
-}())
+}());
